34fdb1c1e8596715212d6222ef04363c6656d594
fegzycole <fergusoniyara@gmail.com>
2019-12-03 17:08:46 +0100

    add the bubble_sort_by method

--- main.rb
+++ main.rb
@@ -6,15 +6,32 @@ def bubble_sort(array)
     index = 0
     extra_counter = counter
     while index < extra_counter
-      if array[index] > array[index + 1]
-        array[index], array[index + 1] = array[index + 1], array[index]
-      end
+      array[index], array[index + 1] = array[index + 1], array[index] if array[index] > array[index + 1]
       index += 1
     end
-    counter -=1
+    counter -= 1
+  end
+  array
+end
+
+def bubble_sort_by(array)
+  counter = array.length - 1
+  array.each do
+    index = 0
+    extra_counter = counter
+    while index < extra_counter
+      array[index], array[index + 1] = array[index + 1], array[index] if yield(array[index], array[index + 1]).positive?
+      index += 1
+    end
+    counter -= 1
   end
   array
 end
 
 array = [35, 9, 7, 13, 1, 36, 2, 83, 10]
+
 p bubble_sort(array)
+
+new_array = %w[Hey Hello Hi]
+
+p bubble_sort_by(new_array) { |left, right| left.length - right.length }
