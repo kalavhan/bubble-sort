7421c673cc7aa073b04e8eb2bcf5ff5570c28d43
kalavhan <47485749+kalavhan@users.noreply.github.com>
2019-12-03 08:51:23 -0700

    bubble sort modified

--- main.rb
+++ main.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: false
 
 def bubble_sort(array)
+  counter = array.length - 1
   array.each do
-    array.each_with_index do |_value, index|
-      break unless array[index + 1]
-      next if array[index] < array[index + 1]
-
-      lesser = array[index + 1]
-      array[index + 1] = array[index]
-      array[index] = lesser
+    index = 0
+    extra_counter = counter
+    while index < extra_counter
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+      end
+      index += 1
     end
+    counter -=1
   end
   array
 end
+
+array = [35, 9, 7, 13, 1, 36, 2, 83, 10]
+p bubble_sort(array)
